4157da68f7b21862fce52452541c8ae292619a33
AndreiGudz <andrei.gudz.gudz@mail.ru>
2025-02-19 14:01:24 +0300

    Добавлен OrderListBox
    Готовая работа

--- OrderForm.Designer.cs
+++ OrderForm.Designer.cs
@@ -56,6 +56,7 @@
             label5 = new System.Windows.Forms.Label();
             startOrderButton = new System.Windows.Forms.Button();
             OrderlistBox = new System.Windows.Forms.ListBox();
+            StatusTextBox = new System.Windows.Forms.TextBox();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             SuspendLayout();
@@ -63,20 +64,21 @@
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.4705887F));
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 115F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
             tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 2);
             tableLayoutPanel1.Controls.Add(label5, 0, 0);
             tableLayoutPanel1.Controls.Add(startOrderButton, 1, 2);
             tableLayoutPanel1.Controls.Add(OrderlistBox, 1, 1);
+            tableLayoutPanel1.Controls.Add(StatusTextBox, 1, 0);
             tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 3;
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.41573F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.58427F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.01425F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.98575F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75F));
             tableLayoutPanel1.Size = new System.Drawing.Size(788, 476);
             tableLayoutPanel1.TabIndex = 0;
             // 
@@ -96,12 +98,12 @@
             tableLayoutPanel2.Controls.Add(treeView1, 1, 1);
             tableLayoutPanel2.Controls.Add(addEquipPanel, 3, 1);
             tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            tableLayoutPanel2.Location = new System.Drawing.Point(3, 73);
+            tableLayoutPanel2.Location = new System.Drawing.Point(3, 59);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 2;
             tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            tableLayoutPanel2.Size = new System.Drawing.Size(667, 328);
+            tableLayoutPanel2.Size = new System.Drawing.Size(624, 338);
             tableLayoutPanel2.TabIndex = 1;
             // 
             // checkedListBox2
@@ -109,10 +111,11 @@
             checkedListBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             checkedListBox2.Font = new System.Drawing.Font("Arial Narrow", 14.1428576F);
             checkedListBox2.FormattingEnabled = true;
+            checkedListBox2.HorizontalScrollbar = true;
             checkedListBox2.Items.AddRange(new object[] { "дуб", "береза", "ель", "сосна", "лиственница" });
-            checkedListBox2.Location = new System.Drawing.Point(335, 42);
+            checkedListBox2.Location = new System.Drawing.Point(315, 42);
             checkedListBox2.Name = "checkedListBox2";
-            checkedListBox2.Size = new System.Drawing.Size(160, 283);
+            checkedListBox2.Size = new System.Drawing.Size(150, 293);
             checkedListBox2.TabIndex = 6;
             checkedListBox2.ItemCheck += checkedListBox2_ItemCheck;
             // 
@@ -120,9 +123,9 @@
             // 
             label4.Dock = System.Windows.Forms.DockStyle.Fill;
             label4.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            label4.Location = new System.Drawing.Point(501, 0);
+            label4.Location = new System.Drawing.Point(471, 0);
             label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(163, 39);
+            label4.Size = new System.Drawing.Size(150, 39);
             label4.TabIndex = 4;
             label4.Text = "Дополнительное\r\nоборудование";
             label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -131,9 +134,9 @@
             // 
             label3.Dock = System.Windows.Forms.DockStyle.Fill;
             label3.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            label3.Location = new System.Drawing.Point(335, 0);
+            label3.Location = new System.Drawing.Point(315, 0);
             label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(160, 39);
+            label3.Size = new System.Drawing.Size(150, 39);
             label3.TabIndex = 3;
             label3.Text = "Материалы";
             label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -142,9 +145,9 @@
             // 
             label1.Dock = System.Windows.Forms.DockStyle.Fill;
             label1.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            label1.Location = new System.Drawing.Point(169, 0);
+            label1.Location = new System.Drawing.Point(159, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(160, 39);
+            label1.Size = new System.Drawing.Size(150, 39);
             label1.TabIndex = 2;
             label1.Text = "цвет";
             label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -155,7 +158,7 @@
             label2.Font = new System.Drawing.Font("Arial Narrow", 12F);
             label2.Location = new System.Drawing.Point(3, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(160, 39);
+            label2.Size = new System.Drawing.Size(150, 39);
             label2.TabIndex = 1;
             label2.Text = "Асортимент";
             label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -165,10 +168,11 @@
             checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             checkedListBox1.Font = new System.Drawing.Font("Arial Narrow", 14.1428576F);
             checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.HorizontalScrollbar = true;
             checkedListBox1.Items.AddRange(new object[] { "Стандартные модели", "Модели эконом класса", "Модели класса Люкс", "Стандартная комплектация" });
             checkedListBox1.Location = new System.Drawing.Point(3, 42);
             checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new System.Drawing.Size(160, 283);
+            checkedListBox1.Size = new System.Drawing.Size(150, 293);
             checkedListBox1.TabIndex = 0;
             checkedListBox1.ItemCheck += CheckedListBox_OneItemCheck;
             // 
@@ -177,7 +181,7 @@
             treeView1.CheckBoxes = true;
             treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
             treeView1.Font = new System.Drawing.Font("Arial Narrow", 14.1428576F);
-            treeView1.Location = new System.Drawing.Point(169, 42);
+            treeView1.Location = new System.Drawing.Point(159, 42);
             treeView1.Name = "treeView1";
             treeNode1.BackColor = System.Drawing.Color.Brown;
             treeNode1.Name = "Узел0_1";
@@ -218,25 +222,25 @@
             treeNode13.Name = "Узел1";
             treeNode13.Text = "Другие цвета";
             treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] { treeNode7, treeNode13 });
-            treeView1.Size = new System.Drawing.Size(160, 283);
+            treeView1.Size = new System.Drawing.Size(150, 293);
             treeView1.TabIndex = 5;
-            treeView1.BeforeCheck += TreeView1_BeforeCheck;
+            treeView1.AfterCheck += treeView1_AfterCheck;
             // 
             // addEquipPanel
             // 
             addEquipPanel.BackColor = System.Drawing.Color.WhiteSmoke;
             addEquipPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            addEquipPanel.Location = new System.Drawing.Point(501, 42);
+            addEquipPanel.Location = new System.Drawing.Point(471, 42);
             addEquipPanel.Name = "addEquipPanel";
-            addEquipPanel.Size = new System.Drawing.Size(163, 283);
+            addEquipPanel.Size = new System.Drawing.Size(150, 293);
             addEquipPanel.TabIndex = 7;
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            flowLayoutPanel1.Location = new System.Drawing.Point(3, 407);
+            flowLayoutPanel1.Location = new System.Drawing.Point(3, 403);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new System.Drawing.Size(667, 66);
+            flowLayoutPanel1.Size = new System.Drawing.Size(624, 70);
             flowLayoutPanel1.TabIndex = 2;
             // 
             // label5
@@ -246,7 +250,7 @@
             label5.Font = new System.Drawing.Font("Arial Narrow", 15.8571434F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
             label5.Location = new System.Drawing.Point(3, 0);
             label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(667, 70);
+            label5.Size = new System.Drawing.Size(624, 56);
             label5.TabIndex = 3;
             label5.Text = "Заказ № 0001";
             label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -254,9 +258,9 @@
             // startOrderButton
             // 
             startOrderButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            startOrderButton.Location = new System.Drawing.Point(676, 407);
+            startOrderButton.Location = new System.Drawing.Point(633, 403);
             startOrderButton.Name = "startOrderButton";
-            startOrderButton.Size = new System.Drawing.Size(109, 66);
+            startOrderButton.Size = new System.Drawing.Size(152, 70);
             startOrderButton.TabIndex = 4;
             startOrderButton.Text = "Начать выполнять заказ";
             startOrderButton.UseVisualStyleBackColor = true;
@@ -266,10 +270,25 @@
             // 
             OrderlistBox.Dock = System.Windows.Forms.DockStyle.Fill;
             OrderlistBox.FormattingEnabled = true;
-            OrderlistBox.Location = new System.Drawing.Point(676, 73);
+            OrderlistBox.HorizontalScrollbar = true;
+            OrderlistBox.Location = new System.Drawing.Point(633, 59);
             OrderlistBox.Name = "OrderlistBox";
-            OrderlistBox.Size = new System.Drawing.Size(109, 328);
+            OrderlistBox.Size = new System.Drawing.Size(152, 338);
             OrderlistBox.TabIndex = 5;
+            // 
+            // StatusTextBox
+            // 
+            StatusTextBox.BackColor = System.Drawing.SystemColors.Info;
+            StatusTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            StatusTextBox.Enabled = false;
+            StatusTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
+            StatusTextBox.Location = new System.Drawing.Point(633, 3);
+            StatusTextBox.Multiline = true;
+            StatusTextBox.Name = "StatusTextBox";
+            StatusTextBox.Size = new System.Drawing.Size(152, 50);
+            StatusTextBox.TabIndex = 6;
+            StatusTextBox.Text = "Статус заказа\r\nРаботы не начаты";
+            StatusTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // OrderForm
             // 
@@ -303,5 +322,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button startOrderButton;
         private System.Windows.Forms.ListBox OrderlistBox;
+        private System.Windows.Forms.TextBox StatusTextBox;
     }
 }
--- OrderForm.cs
+++ OrderForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,11 +14,23 @@ namespace yacht_yard_practic
 {
     public partial class OrderForm : Form
     {
+        bool canMakeOrder = true;
+        int machtCount = 0;
+        int price = 100;
+        string model = string.Empty;
+        string material = string.Empty;
+        public List<string> boatColor = new List<string>();
+        public List<string> boatAddEquip = new List<string>();
         public OrderForm()
         {
             InitializeComponent();
             addPartners();
-            this.checkedListBox1.HorizontalScrollbar = true;
+
+            foreach (TreeNode rootNode in treeView1.Nodes)
+            {
+                // Раскрываем все дочерние элементы
+                rootNode.ExpandAll();
+            }
         }
 
         void addPartners()
@@ -46,23 +59,17 @@ namespace yacht_yard_practic
             Application.Exit();
         }
 
-        private void TreeView1_BeforeCheck(object sender, TreeViewCancelEventArgs e)
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
         {
             TreeView _treeView = sender as TreeView;
             // Временно отключаем обработчик события
-            _treeView.BeforeCheck -= TreeView1_BeforeCheck;
+            _treeView.AfterCheck -= treeView1_AfterCheck;
 
             try
             {
-                // Снимаем отметку со всех остальных элементов
-                for (int i = 0; i < _treeView.Nodes.Count; i++)
-                {
-                    if (_treeView.Nodes[i].Level == 0) // Снимать отметку с корневых элементов 
-                    {
-                        _treeView.Nodes[i].Checked = false;
-                    }
-                }
-
+                // Снимаем отметку со всех корневых элементов
+                foreach (var item in _treeView.Nodes)
+                    (item as TreeNode).Checked = false;
             }
             catch (Exception ex)
             {
@@ -71,8 +78,15 @@ namespace yacht_yard_practic
             finally
             {
                 // Восстанавливаем обработчик события в любом случае
-                _treeView.BeforeCheck += TreeView1_BeforeCheck;
+                _treeView.AfterCheck += treeView1_AfterCheck;
             }
+            if (e.Node is not null)
+                if (e.Node.Checked && e.Node.Parent is not null)
+                    boatColor.Add(e.Node.Text);
+                else
+                    boatColor.Remove(e.Node.Text);
+
+            OrderListBoxFill();
         }
 
         private void CheckedListBox_OneItemCheck(object sender, ItemCheckEventArgs e)
@@ -98,12 +112,11 @@ namespace yacht_yard_practic
 
             clb.ItemCheck += CheckedListBox_OneItemCheck;
 
-            // Обновляем панель после изменения
             clb.BeginInvoke(
                 new Action(() =>
-                    {
-                        UpdateAddEquipPanel(clb.CheckedItems.Count > 0 ? clb.CheckedItems[0] : null);
-                    }
+                {
+                    UpdateAddEquipPanel(clb.CheckedItems.Count > 0 ? clb.CheckedItems[0] : null);
+                }
                     )
                 );
         }
@@ -116,17 +129,25 @@ namespace yacht_yard_practic
                 addEquipPanel.Controls.Remove(ctrl);
                 ctrl.Dispose();
             }
+            boatAddEquip.Clear();
 
             if (selectedItem == null) return;
 
             // Создаем новый список
             CheckedListBox newList = new CheckedListBox();
             newList.Dock = DockStyle.Fill;
+            newList.Font = new System.Drawing.Font("Arial Narrow", 14.1428576F);
+            newList.HorizontalScrollbar = true;
+            newList.ItemCheck += AddEquipCLB_ItemCheck;
 
             // Добавляем элементы в зависимости от выбранного элемента
             newList.Items.AddRange(GetItemsForEquipment(selectedItem.ToString()));
 
             addEquipPanel.Controls.Add(newList);
+
+            boatAddEquip.Clear();
+            model = selectedItem.ToString();
+            OrderListBoxFill();
         }
 
         private string[] GetItemsForEquipment(string equipment)
@@ -140,17 +161,52 @@ namespace yacht_yard_practic
             switch (equipment)
             {
                 case "Стандартные модели":
-                    return new[] { "Черпак", "Весла", "Навес", "Зонтик", "Спасательный жилет" };
+                    price = 200;
+                    machtCount = 1;
+                    return new[]
+                    {
+                        "Черпак", "Весла", "Навес", "Зонтик",
+                        "Спасательный жилет", "Дополнительная мачта"
+                    };
                 case "Модели эконом класса":
-                    return new[] { "Чек (платный)" };
+                    price = 10;
+                    machtCount = 0;
+                    return new[]
+                    {
+                        "Напутствие (бесплатно)"
+                    };
                 case "Модели класса Люкс":
-                    return new[] { "Черпак", "Весла", "Навес", "Зонтик", "Спасательный жилет",
-                        "Холодильник", "Бассейн", "Выдвижной пирс", "Бар"};
+                    price = 1000;
+                    machtCount = 3;
+                    return new[]
+                    {
+                        "Черпак", "Весла", "Навес", "Зонтик",
+                        "Спасательный жилет", "Холодильник", "Бассейн",
+                        "Выдвижной пирс", "Бар", "Дополнительная мачта"
+                    };
                 case "Стандартная комплектация":
-                    return new[] { "Черпак", "Весла", "Спасательный жилет" };
+                    price = 300;
+                    machtCount = 1;
+                    return new[]
+                    {
+                        "Черпак", "Весла", "Спасательный жилет", "Дополнительная мачта"
+                    };
                 default:
                     return Array.Empty<string>();
             }
+        }
+
+        private void AddEquipCLB_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            CheckedListBox clb = sender as CheckedListBox;
+            if (clb == null) return;
+
+            if (e.NewValue == CheckState.Checked)
+                boatAddEquip.Add(clb.GetItemText(clb.Items[e.Index]));
+            else
+                boatAddEquip.Remove(clb.GetItemText(clb.Items[e.Index]));
+
+            OrderListBoxFill();
         }
 
         private void checkedListBox2_ItemCheck(object sender, ItemCheckEventArgs e)
@@ -175,12 +231,78 @@ namespace yacht_yard_practic
             }
 
             clb.ItemCheck += checkedListBox2_ItemCheck;
+
+            material = clb.GetItemText(clb.Items[e.Index]);
+            OrderListBoxFill();
         }
 
-        // пока пустой метод
         private void startOrderButton_Click(object sender, EventArgs e)
         {
+            CheckedListBox? addEquipCLP = addEquipPanel.Controls.OfType<CheckedListBox>().FirstOrDefault();
+            if (checkedListBox1.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Выберите модель", "предупреждение");
+                return;
+            }
+            if (checkedListBox2.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Выберите материал", "предупреждение");
+                return;
+            }
 
+            if (addEquipCLP is not null && canMakeOrder)
+            {
+                var result = MessageBox.Show(
+                        "Начав заказ, вы не сможете от него отказаться\n\r" +
+                        "Вы хотите продолжить?",
+                        "Сообщение",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information,
+                        MessageBoxDefaultButton.Button1
+                    );
+                if (result == DialogResult.Yes)
+                { 
+                    StartOrder();
+                    this.Focus();
+                }
+            }
+        }
+
+        private void StartOrder()
+        {
+            canMakeOrder = false;
+            startOrderButton.Enabled = false;
+            startOrderButton.Text = "Заказ выполняется";
+
+            StatusTextBox.Text = "Статус заказа\r\nНачато производство";
+
+            OrderListBoxFill();
+        }
+
+        void OrderListBoxFill()
+        {
+            OrderlistBox.Items.Clear();
+            OrderlistBox.Items.Add("Модель: ");
+            OrderlistBox.Items.Add($" - {model}");
+
+            OrderlistBox.Items.Add("Цвета: ");
+            if (boatColor.Count() == 0)
+                OrderlistBox.Items.Add($" - Цвет древесины");
+            else
+                foreach (var color in boatColor)
+                    OrderlistBox.Items.Add($" - {color}");
+
+            OrderlistBox.Items.Add("Материал: ");
+            OrderlistBox.Items.Add($" - {material}");
+
+            OrderlistBox.Items.Add("Дополнительное оборудование:");
+            foreach (var equip in boatAddEquip)
+                OrderlistBox.Items.Add($" - {equip}");
+
+            OrderlistBox.Items.Add("Гарантия: 25 лет");
+            OrderlistBox.Items.Add($"Количество мачт: {machtCount}");
+            OrderlistBox.Items.Add("Базовая цена: ");
+            OrderlistBox.Items.Add($"{price} - без дополнительных аксессуаров и комплектующих");
         }
     }
 }
